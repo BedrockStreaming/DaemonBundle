41f82bad85d6072cadfb973c014ea48f43f63c2d
Olivier Mansour <olivier.mansour@m6.fr>
2014-05-05 17:17:38 +0200

    phpcs

--- src/M6Web/Bundle/DaemonBundle/Command/StopLoopException.php
+++ src/M6Web/Bundle/DaemonBundle/Command/StopLoopException.php
@@ -4,6 +4,7 @@ namespace M6Web\Bundle\DaemonBundle\Command;
 
 /**
  * Class StopLoopException - allow user to interrupt the daemon loop
+ *
  * @package M6Web\Bundle\DaemonBundle\Command
  */
 class StopLoopException extends \Exception
